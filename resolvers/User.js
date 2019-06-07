9af553523215588722afe05cabd5c851e2be51a4
Stanley Huang <huang.stanley050@gmail.com>
2019-06-07 11:51:33 +1000

    discovered issue graphql keep on sending fetch request

--- resolvers/User.js
+++ resolvers/User.js
@@ -1,6 +1,11 @@
+import Post from "../models/post";
+import Comment from "../models/comment";
+
 const User = {
-  posts: (parent, args, {db}, info) =>
-    db.posts.filter(post => parent.id === post.author),
+  posts: async (parent, args, {db}, info) => {
+    return await Post.find({author: parent.id});
+    //db.posts.filter(post => parent.id === post.author);
+  },
   comments: (parent, args, {db}, info) =>
     db.comments.filter(comment => parent.id === comment.author)
 };
